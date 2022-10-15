dd86a8c8bd91883a5ab074906d4622a13b607a47
AryehSchoefer <aryeh.shmuel@googlemail.com>
2022-10-15 20:15:11 +0200

    added page transitions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
+import FadeOutToBottom from "../components/FadeOutToBottom";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -11,7 +12,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <FadeOutToBottom>
+        <Component {...pageProps} />
+      </FadeOutToBottom>
     </SessionProvider>
   );
 };
